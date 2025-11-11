9434869ccf80451f7e92c60b4c8f307d1ae0784e
SARAVANAKUMAR_CCL <saravanakumar.m@crystalcodelabs.com>
2025-11-11 12:50:01 +0530

    MODIFY

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => (
           <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png" /></a>
         </div>
         <div className="social-item">
-          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
+          <a href="https://www.linkedin.com/feed/"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
         </div>
       </div>
     </div>
