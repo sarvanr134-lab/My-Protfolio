29e558eda4b1ee08ff06bd3ec2fc7d9fc6b5bbb8
sarvanr134-lab <sarvanr134@gmail.com>
2026-04-04 14:33:15 +0530

    new updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import img1 from "../assets/Event_management.png";
+import img2 from "../assets/fuel-management-system.webp";
 import Netflix_banner from "../assets/netflix_banner.jpg"
 
 const Projects = () => (
@@ -39,6 +40,30 @@ const Projects = () => (
         <div className="project-item">
           <div className="project-info">
             <h1>Project 2</h1>
+            <h2>Fuel Station Management</h2>
+            <p>
+              A fuel station management system built with React, Node.js, and MongoDB 
+              for efficient fuel station operations and management.
+            </p>
+            <a
+              href="https://yourportfolio.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="view-btn">View Project</button>
+            </a>
+          </div>
+          <div className="project-img">
+            <img src={img2} alt="Project 2" />
+          </div>
+        </div>
+        
+
+        {/* --- Project 3 --- */}
+        
+        <div className="project-item">
+          <div className="project-info">
+            <h1>Project 2</h1>
             <h2>Netflix Clone</h2>
             <p>
              Building Netflix clone 
@@ -55,30 +80,11 @@ const Projects = () => (
             <img src={Netflix_banner} alt="Project 2" />
           </div>
         </div>
-
-        {/* --- Project 3 --- */}
-        <div className="project-item">
-          <div className="project-info">
-            <h1>Project 3</h1>
-            <h2>Portfolio Website</h2>
-            <p>
-              A personal portfolio built with React and Tailwind CSS showcasing
-              projects and skills.
-            </p>
-            <a
-              href="https://yourportfolio.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className="view-btn">View Project</button>
-            </a>
-          </div>
-          <div className="project-img">
-            <img src={img1} alt="Project 3" />
-          </div>
-        </div>
       </div>
     </div>
+
+
+
   </section>
 );
 
